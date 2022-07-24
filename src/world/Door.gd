74e2aff332cda940db9b69f4c7dbdcf1795b8d45
myin <minyin142p@gmail.com>
2022-07-24 16:36:01 +0200

    add light occlusion

--- src/world/Door.gd
+++ src/world/Door.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var door_sprite := $Sprite
 onready var open_sound := $OpenDoor
 onready var close_sound := $CloseDoor
+onready var occluder := $LightOccluder2D
 
 func _ready():
 	door_sprite.hide()
@@ -11,8 +12,10 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	door_sprite.show()
 	open_sound.play()
+	occluder.hide()
 
 
 func _on_Area2D_body_exited(body):
 	door_sprite.hide()
 	close_sound.play()
+	occluder.show()
