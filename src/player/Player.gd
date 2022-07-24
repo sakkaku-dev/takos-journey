ac9b2b83c0014f1e9636e100548cb28219216209
myin <minyin142p@gmail.com>
2022-07-24 12:38:35 +0200

    fix jumping

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -3,27 +3,23 @@ extends KinematicBody2D
 export var acceleration := 800
 export var speed := 200
 
-export var jump_force := 800
-export var gravity_force := 2000
+export var jump_force := 300
+export var initial_gravity_force := 9.8
 
 onready var input := $PlayerInput
 onready var sprite := $Sprite
 onready var anim := $AnimationPlayer
 
-var gravity := Vector2.DOWN
 var velocity := Vector2.ZERO
 
 func _physics_process(delta):
-	var desired_velocity = _get_motion() * speed
-	velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	var motion_x = input.get_action_strength("move_right") - input.get_action_strength("move_left")
+	velocity.x = move_toward(velocity.x, motion_x * speed, acceleration * delta)
+	velocity += Vector2.DOWN * initial_gravity_force
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	if not is_on_floor():
-		velocity += gravity * gravity_force * delta
-	
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP, true, 4, deg2rad(70))
-
-	if desired_velocity:
-		sprite.scale.x = -1 if desired_velocity.x < 0 else 1
+	if motion_x:
+		sprite.scale.x = -1 if motion_x < 0 else 1
 	
 	if velocity:
 		if is_on_floor():
@@ -37,10 +33,12 @@ func _physics_process(delta):
 		anim.play("Idle")
 
 
-func _get_motion():
-	return Vector2(input.get_action_strength("move_right") - input.get_action_strength("move_left"), 0)
-
-
 func _on_PlayerInput_just_pressed(action):
 	if action == "jump" and is_on_floor():
-		velocity += Vector2.UP * jump_force
+		velocity.y = -jump_force
+
+
+func _on_PlayerInput_just_released(action):
+	if action == "jump":
+		if velocity.y < 0:
+			velocity.y = 0
