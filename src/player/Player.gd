4d2a63aebf067ede877b063082e4ac17ca70f2eb
myin <minyin142p@gmail.com>
2022-07-27 17:13:26 +0200

    add basic slide

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,7 +1,15 @@
 class_name Player extends KinematicBody2D
 
+enum {
+	MOVE,
+	SLIDE,
+}
+
 export var acceleration := 800
 export var speed := 200
+
+export var slide_force := 5
+export var slide_deacceleration := 800
 
 export var jump_force := 300
 export var initial_gravity_force := 9.8
@@ -15,20 +23,41 @@ onready var land_sound := $LandSound
 onready var jump_sound := $JumpSound
 
 var velocity := Vector2.ZERO
+var state = MOVE
+
+func _process(_delta):
+	anim_tree.update_animation(self)
+
 
 func _physics_process(delta):
 	var was_grounded = is_on_floor()
 	
-	var motion_x = input.get_action_strength("move_right") - input.get_action_strength("move_left")
-	velocity.x = move_toward(velocity.x, motion_x * speed, acceleration * delta)
+	match state:
+		MOVE: _move(delta)
+		SLIDE: _slide(delta)
+	
 	velocity += Vector2.DOWN * initial_gravity_force
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	_update_sound(was_grounded)
+
+
+func _move(delta: float):
+	var motion_x = input.get_action_strength("move_right") - input.get_action_strength("move_left")
+	velocity.x = move_toward(velocity.x, motion_x * speed, acceleration * delta)
+	
 	if motion_x:
 		sprite.scale.x = -1 if motion_x < 0 else 1
-		
-	anim_tree.update_animation(self)
+
+
+func _slide(delta: float):
+	if abs(velocity.x) <= 0:
+		state = MOVE
 	
+	velocity.x = move_toward(velocity.x, 0, slide_deacceleration * delta)
+
+
+func _update_sound(was_grounded: bool):
 	# TODO: make better
 	if is_on_floor():
 		if velocity:
@@ -46,6 +75,10 @@ func _on_PlayerInput_just_pressed(action):
 	if action == "jump":
 		velocity.y = -jump_force
 		jump_sound.play()
+	elif action == "slide" and state != SLIDE:
+		state = SLIDE
+		velocity.x *= slide_force
+
 
 func _on_PlayerInput_just_released(action):
 	if action == "jump":
--- src/player/PlayerAnimation.gd
+++ src/player/PlayerAnimation.gd
@@ -11,7 +11,7 @@ const RISING = "parameters/conditions/rising"
 const MOVING = "parameters/grounded/conditions/moving"
 const NOT_MOVING = "parameters/grounded/conditions/not_moving"
 const SLIDING = "parameters/grounded/conditions/sliding"
-const NOT_SLIDING = "parameters/grounded/conditions/sliding"
+const NOT_SLIDING = "parameters/grounded/conditions/not_sliding"
 
 
 func update_animation(player: Player):
@@ -24,3 +24,5 @@ func update_animation(player: Player):
 	set(MOVING, abs(player.velocity.x) > 0)
 	set(NOT_MOVING, not get(MOVING))
 
+	set(SLIDING, player.state == Player.SLIDE)
+	set(NOT_SLIDING, not get(SLIDING))
