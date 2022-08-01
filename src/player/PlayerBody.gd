b80a435be0863a88e19d645ef636db13a3d54ca5
myin <minyin142p@gmail.com>
2022-08-01 20:45:44 +0200

    add book

--- src/player/PlayerBody.gd
+++ src/player/PlayerBody.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 onready var tentacles := $Tentacles
+onready var book := $Book
 
 onready var player: Player = owner
 
 func _process(delta):
 	scale.x = sign(player.face_dir.x)
 	tentacles.visible = player.tentacle_mode
+
+func _on_Tentacles_visibility_changed():
+	book.animation = "open" if tentacles.visible else "close"
